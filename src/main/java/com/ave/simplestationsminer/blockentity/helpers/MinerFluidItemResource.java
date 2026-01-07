21b456e34b94d06ffa59ccc80b8c34ab0a313c14
Andrejev Vladimir <andrejevve@gmail.com>
2026-01-07 23:33:09 +0200

    Fix fluid item usage

--- src/main/java/com/ave/simplestationsminer/blockentity/helpers/MinerFluidItemResource.java
+++ src/main/java/com/ave/simplestationsminer/blockentity/helpers/MinerFluidItemResource.java
@@ -2,6 +2,9 @@ package com.ave.simplestationsminer.blockentity.helpers;
 
 import com.ave.simplestationscore.resources.FluidItemResource;
 
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Items;
+
 public class MinerFluidItemResource extends FluidItemResource {
 
     public MinerFluidItemResource(int max, int usage, String key) {
@@ -11,5 +14,10 @@ public class MinerFluidItemResource extends FluidItemResource {
     @Override
     public boolean useEveryTick() {
         return false;
+    }
+
+    @Override
+    public int getIncrement(Item item) {
+        return item.equals(Items.REDSTONE_BLOCK) || item.equals(Items.LAPIS_BLOCK) ? 9 : 1;
     }
 }
